77f4b2b4cfb6d11e0ea6984705104432d2aacab4
poli2pro <monopolidavid@gmail.com>
2016-05-12 10:33:18 +0200

    controllo sul linguaggio dell'utente

---
+++ Filtro Parolacce/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Filtro_Parolacce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnPubblica = new System.Windows.Forms.Button();
+            this.txtTesto = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(793, 558);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnPubblica);
+            this.tabPage1.Controls.Add(this.txtTesto);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(785, 532);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Posta qualcosa!";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnPubblica
+            // 
+            this.btnPubblica.Location = new System.Drawing.Point(689, 501);
+            this.btnPubblica.Name = "btnPubblica";
+            this.btnPubblica.Size = new System.Drawing.Size(93, 25);
+            this.btnPubblica.TabIndex = 1;
+            this.btnPubblica.Text = "Pubblica";
+            this.btnPubblica.UseVisualStyleBackColor = true;
+            this.btnPubblica.Click += new System.EventHandler(this.btnPubblica_Click);
+            // 
+            // txtTesto
+            // 
+            this.txtTesto.Location = new System.Drawing.Point(6, 91);
+            this.txtTesto.Name = "txtTesto";
+            this.txtTesto.Size = new System.Drawing.Size(773, 404);
+            this.txtTesto.TabIndex = 0;
+            this.txtTesto.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(785, 532);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Blog page";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 584);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnPubblica;
+        private System.Windows.Forms.RichTextBox txtTesto;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ Filtro Parolacce/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Filtro_Parolacce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool scrittoQualcosa()
+        {
+            if (txtTesto.Text == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+
+
+
+
+        private void btnPubblica_Click(object sender, EventArgs e)
+        {
+            bool contr = scrittoQualcosa();
+            if (!contr)
+            {
+                MessageBox.Show("ERRORE: per postare la tua storia, inserisci del testo");
+                return;
+            }
+
+            bool linguaTesto = linguaInglese(txtTesto.Text);
+            if (linguaTesto)
+                label1.Text = "Inglese";
+            else
+                label1.Text = "Italiano";
+
+        }
+
+
+
+
+        bool parolaInglese(string parola)
+        {
+            int primo, medio, ultimo;
+            primo = 0;
+            ultimo = paroleInglesi.Length - 1;
+            while (primo <= ultimo)
+            {
+                medio = (primo + ultimo) / 2;
+                string parolaMedia = paroleInglesi[medio];
+                if (parolaMedia == parola)
+                {
+                    return true;
+                }
+                if (string.Compare(parolaMedia, parola) > 0)
+                {
+                    ultimo = medio - 1;
+                    
+                }
+                else
+                {
+                    primo = medio + 1;
+                    
+                }
+            }
+            return false;
+        }
+
+
+        bool linguaInglese(string testo)
+        {
+            int paroleTrov = 0;
+            int paroleNonTrov = 0;
+            
+            string[] parole = testo.Split(' ');         //ARRAY DELLE PAROLE SCRITTE DALL'UTENTE
+            for(int i = 0; i < parole.Length; i++)
+            {
+                bool controllo = parolaInglese(parole[i]);
+                if (controllo)
+                {
+                    paroleTrov++;
+                }
+                else if(!controllo)
+                    paroleNonTrov++;
+            }
+
+
+            if(paroleTrov>=paroleNonTrov)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+
+        string[] paroleInglesi = new string[0];
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream parole = new FileStream("Lista parole Inglese.txt", FileMode.Open);
+            StreamReader lettura = new StreamReader(parole);
+            while (!lettura.EndOfStream)
+            {
+                string word = lettura.ReadLine();
+                Array.Resize(ref paroleInglesi, paroleInglesi.Length + 1);
+                paroleInglesi[paroleInglesi.Length - 1] = word;
+            }
+            lettura.Close();
+            parole.Close();
+
+
+
+        }
+
+      
+    }
+}
