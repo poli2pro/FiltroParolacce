adf31788fcf8277ee4e02f63662c63c2ba43b069
ZedGod <anesbajji@yahoo.it>
2016-05-12 10:49:21 +0200

    bottone test

--- Filtro Parolacce/Form1.Designer.cs
+++ Filtro Parolacce/Form1.Designer.cs
@@ -33,8 +33,10 @@
             this.btnPubblica = new System.Windows.Forms.Button();
             this.txtTesto = new System.Windows.Forms.RichTextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -79,6 +81,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button1);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -86,6 +89,15 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Blog page";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -98,6 +110,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -109,6 +122,7 @@
         private System.Windows.Forms.Button btnPubblica;
         private System.Windows.Forms.RichTextBox txtTesto;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
